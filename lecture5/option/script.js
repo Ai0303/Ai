a00e961e3f5d355a2671ea3c44a9b4dde724d31a
Ai0303 <ai3apple@gmail.com>
2022-11-11 22:17:38 +0900

    waffle_foundation

---
+++ lecture5/option/script.js
@@ -0,0 +1,13 @@
+'use strict'
+// Please don't delete the 'use strict' line above
+console.log("hello world!");
+
+// const color_map = new Map();
+// color_map.set("赤", 12);
+// color_map.set("青", 15);
+
+// console.log(color.map("青"));
+// my_map.set
+
+const colors = ["赤", "赤", "青", "白", "青","赤" ];
+const color_map = new Map();
